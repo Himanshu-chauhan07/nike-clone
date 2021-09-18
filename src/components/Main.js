df5a776c71f85cc1cf8ac2a27542347ece7d1b09
alphaHimanshu <Himanshu1640133@akgec.ac.in>
2021-09-18 16:43:01 +0530

    changes for second svg on small screen

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,20 +3,30 @@ function Main() {
     <div>
       <div className="d-flex justify-content-center flex-column text-center mb-2">
         <p className="mb-0">SAVE UP TO 40%</p>
-          <a className="dark-link small-link" href="/">
-            Shop All Our New Markdowns
-          </a>
+        <a className="dark-link small-link" href="/">
+          Shop All Our New Markdowns
+        </a>
       </div>
       <div className="container">
-        <img
+      <div className="show-img-large container">
+      <img 
           data-qa="image-media-img"
           alt="Nike. Just Do It"
           className="_32IPZERI _3jm9Bm_E guL_1FMX fit-in-parent"
           src="https://static.nike.com/a/images/f_auto/dpr_1.0/w_1239,c_limit/7a1bedb5-5cfa-4fa0-a38a-397ed691cbc6/nike-just-do-it.jpg"
         />
-        <div className="text-center my-2">
-        <h1>YOUR NIKE CONNECTION</h1>
-        <p>More support ,more inspiration, more Nike!</p>
+      </div>
+       <div className="show-img-small container text-center"> 
+       <img 
+          data-qa="image-media-img"
+          alt="Nike. Just Do It"
+          class="_32IPZERI _3jm9Bm_E guL_1FMX"
+          src="https://static.nike.com/a/images/f_auto/dpr_1.0/w_467,c_limit/69b4c8be-ad46-4b8d-b2a9-e8911358c536/nike-just-do-it.jpg"
+        />
+       </div>
+        <div className="my-2 d-flex justify-content-center flex-column text-center">
+          <h1>YOUR NIKE CONNECTION</h1>
+          <p>More support ,more inspiration, more Nike!</p>
         </div>
       </div>
     </div>
